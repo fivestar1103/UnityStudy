c0b0dba42b4a089fbf280d5e33859b226b129112
Seonghwan Oh <aep@skygg.xyz>
2023-09-09 01:51:25 +0900

    Zombie SinglePlayer Complete

---
+++ Unity-Programming-Essence/14/Zombie/Assets/Scripts/ZombieData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable/ZombieData", fileName = "Zombie Data")]
+public class ZombieData : ScriptableObject
+{
+    public float health = 100f;
+    public float damage = 20f;
+    public float speed = 2f;
+    public Color skinColor = Color.white;
+}
+
---
+++ Unity-Programming-Essence/14/Zombie/Assets/Scripts/ZombieSpawner.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+// 적 게임 오브젝트를 주기적으로 생성
+public class ZombieSpawner : MonoBehaviour {
+    public Zombie zombiePrefab; // 생성할 적 AI
+
+    public ZombieData[] zombieDatas;
+    public Transform[] spawnPoints;
+
+    private List<Zombie> zombies = new List<Zombie>(); // 생성된 적들을 담는 리스트
+    private int wave; // 현재 웨이브
+
+    private void Update() {
+        // 게임 오버 상태일때는 생성하지 않음
+        if (GameManager.instance != null && GameManager.instance.isGameover)
+        {
+            return;
+        }
+
+        // 적을 모두 물리친 경우 다음 스폰 실행
+        if (zombies.Count <= 0)
+        {
+            SpawnWave();
+        }
+
+        // UI 갱신
+        UpdateUI();
+    }
+
+    // 웨이브 정보를 UI로 표시
+    private void UpdateUI() {
+        // 현재 웨이브와 남은 적의 수 표시
+        UIManager.instance.UpdateWaveText(wave, zombies.Count);
+    }
+
+    // 현재 웨이브에 맞춰 적을 생성
+    private void SpawnWave()
+    {
+        wave++;
+
+        int spawnCount = Mathf.RoundToInt(wave * 1.5f);
+
+        for (int i = 0; i < spawnCount; i++)
+        {
+            CreateZombie();
+        }
+    }
+
+    // 적을 생성하고 생성한 적에게 추적할 대상을 할당
+    private void CreateZombie()
+    {
+        ZombieData zombieData = zombieDatas[Random.Range(0, zombieDatas.Length)];
+
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+        Zombie zombie = Instantiate(zombiePrefab, spawnPoint.position, spawnPoint.rotation);
+
+        zombie.Setup(zombieData);
+        
+        zombies.Add(zombie);
+
+        zombie.onDeath += () => zombies.Remove(zombie);
+        zombie.onDeath += () => Destroy(zombie.gameObject, 10f);
+        zombie.onDeath += () => GameManager.instance.AddScore(100);
+    }
+}
